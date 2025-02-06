946a024dd60dd9713ca23da5823b6bda62ca59b7
JustinV-Dev <justin@heureuses.fr>
2025-02-06 17:18:51 +0100

    First commit - Game Loop

---
+++ MyFirst2DGame/src/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame();
+		GamePanel gamePanel = new GamePanel();
+		
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("My First 2D Game");
+		
+	
+		window.add(gamePanel);
+		
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		gamePanel.startGameThread();
+	}
+}
